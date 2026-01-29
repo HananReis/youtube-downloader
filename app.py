de8fe2fd88b4ca27c0ac89e1b738a48be7a76029
ElHanan Sousa dos Reis <elhanansousa65@gmail.com>
2026-01-29 20:11:44 -0300

    adiciona novas páginas html

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
@@ -20,5 +19,4 @@ def contato():
     return render_template("paginas/contanto.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(debug=True)
